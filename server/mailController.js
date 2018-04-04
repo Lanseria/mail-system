7f996cde5b4696a4c70f99579fa8c3ec6ee1f277
Lanseria <zhangchao564265135@hotmail.com>
2018-04-04 14:46:08 +0800

    fix 默认值的问题

--- server/mailController.js
+++ server/mailController.js
@@ -3,10 +3,7 @@ const { transporter, mailOptions } = require('./mail/mailer')
 exports.common = async function (ctx) {
   const { request } = ctx
   const { query } = request
-  mailOptions.to = query.toMail
-  mailOptions.subject = query.subject
-  mailOptions.text = query.text
-  mailOptions.html = query.html
-  const info = await transporter.sendMail(mailOptions)
+  const newMailOpt = { ...mailOptions, ...query}
+  const info = await transporter.sendMail(newMailOpt)
   ctx.body = info
 }
