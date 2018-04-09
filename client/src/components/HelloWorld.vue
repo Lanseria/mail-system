5285dbe45508d7a94ac60f394722489d1753c6db
Lanseria <zhangchao564265135@hotmail.com>
2018-04-09 19:14:56 +0800

    add template of vue

---
+++ client/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="hello">
+    <div class="marker" ref="marker"></div>
+    <button @click="addVueComponents">add</button>
+    <card ref="cardTemplate" v-show="false"/>
+  </div>
+</template>
+
+<script>
+import card from './card'
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    addVueComponents () {
+      console.log(this.$refs.cardTemplate)
+      const helloW = document.createElement('div')
+      helloW.innerHTML = 'hello'
+      this.$refs.marker.appendChild(this.$refs.cardTemplate.$el)
+    }
+  },
+  components: {
+    card
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
